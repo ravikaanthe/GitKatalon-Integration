9852a276805671ecb4baa0e608c5bea1defd26f7
ravik <ravik@10.0.0.58>
2024-08-31 15:57:56 -0400

    Commiting Test Case and Suite

---
+++ Test Suites/Smoke Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>451d6d93-f314-4ff3-b90e-ca4374a88705</testSuiteGuid>
+</TestSuiteEntity>
