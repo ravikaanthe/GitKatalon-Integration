0f2bf3525a3317be6b6e58c46563d823cbd1b58c
ravik <ravik@10.0.0.58>
2024-10-09 22:12:48 -0400

    Test Scripts related to Visual Testing Lesson 03

---
+++ Test Suites/Visual Testing Lesson 03 - TestOps.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Visual Testing Lesson 03 - TestOps</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ccafda15-391f-4b05-9fb3-c04a9a555efb</testSuiteGuid>
+   <testCaseLink>
+      <guid>baa26fc1-0542-4138-9e41-f7adafc37274</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Visual Testing/Visual Testing - Lesson 03 Test Ops</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
